8211ac2317c8e94fe0293baa9eca3e09a62c8b38
Laura Knight <ljknight@gmail.com>
2015-09-13 16:32:30 -0700

    Uncommented a require to run properly on server

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 /* jshint camelcase: false */
 /*jshint quotmark: false */
-// var mysql = require('mysql');
+var mysql = require('mysql');
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
